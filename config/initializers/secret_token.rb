b59f796c31152104ea5ec86fb12f2c1d365ea0af
Jorge <jesm00@hotmail.com>
2013-08-16 10:12:35 -0500

    Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PesoIdeal::Application.config.secret_key_base = '82b12686e1b7280abb05f9d23615703b4b0a5056a1601bc4aa0067bfe59e7577a7fd042ed33ff1dd8b53da6b92f68d4bee27c47fa3a5be2edd918c84123fda7b'
